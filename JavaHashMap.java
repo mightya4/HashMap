53eee9e665da54a297c5193a760bb2a9e9412356
Akeem <akeem.mighty@gmail.com>
2022-10-16 21:10:02 -0400

    Imported HashMap Class + Created HashMap Object + Added Books To Map

--- JavaHashMap.java
+++ JavaHashMap.java
@@ -1,3 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class JavaHashMap {
-    
+    public static void main(string[] args) {
+        HashMap<Integer, String> hm = new HashMap<Integer, String>();
+        hm.put(201, "To Kill A Mockingbird");
+        hm.put(202, "The Hunger Games");
+        hm.put(203, "Water for Elephants");
+        hm.put(204, "Lord of the Flies");
+        hm.put(205, "The Da Vinci Code");
+
+        for(Map.Entry<Integer, String> set: hm.entrySet()) {
+            System.out.println(i);
+        }
+    }
 }
