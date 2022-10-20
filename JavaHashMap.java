ccd34e367afbdca5b8fb5823707a4358be17434c
Akeem <akeem.mighty@gmail.com>
2022-10-19 23:47:19 -0400

    Updated HashMap Mapping Output + Output HashMap Keys + Output HashMap Values

--- JavaHashMap.java
+++ JavaHashMap.java
@@ -3,7 +3,7 @@ import java.util.Map;
 import java.util.Set;
 
 public class JavaHashMap {
-    public static void main(string[] args) {
+    public static void main(String[] args) {
         HashMap<Integer, String> hm = new HashMap<Integer, String>();
         hm.put(201, "To Kill A Mockingbird");
         hm.put(202, "The Hunger Games");
@@ -12,7 +12,18 @@ public class JavaHashMap {
         hm.put(205, "The Da Vinci Code");
 
         for(Map.Entry<Integer, String> set: hm.entrySet()) {
-            System.out.println(i);
+            System.out.println(set);
         }
+
+        System.out.println("\n----------Keys----------");
+        for(int i: hm.keySet()) {
+            System.out.print(i + " | ");
+        }
+
+        System.out.println("\n----------Values----------");
+        for(String j: hm.values()) {
+            System.out.print(j + " | ");
+        }
+
     }
 }
